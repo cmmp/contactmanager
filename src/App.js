5726e80f8704d2c317f3d393116389f2ef901d80
Cassio M. M. Pereira <cassiomartini@gmail.com>
2019-02-23 08:45:30 -0300

    basic components and bootstrap setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The app component</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact name="John Doe" email="jdoe@gmail.com" phone="555 555 555" />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="666 555 555"
+          />
+        </div>
       </div>
     );
   }
